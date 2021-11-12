54ea0b49442e1a4fc2b9f8da3f839ed5d565a5fe
Mikhail Lapitskiy <hiimisha@mail.ru>
2021-11-12 16:37:27 +0300

    prettier

--- src/components/popup-list.jsx
+++ src/components/popup-list.jsx
@@ -16,37 +16,34 @@ const PopupList = ({
         >
             {filteredUsers.length > 0 ? (
                 filteredUsers.map((user) => (
-                        <li
-                            key={`${user.userId}qwerty`}
-                            onClick={
-                                isMulti
-                                    ? onToggle.bind(null, user.userId)
-                                    : handleClick.bind(null, user.userId)
-                            }
-                            className={`${
-                                user.checked ? "checked-user " : ""
-                            }users__list__item`}
-                        >
-                    <span key={user.userName} className="test123">
-                        {user.userName}
-                    </span>
-                            {isMulti && (
-                                <input
-                                    key={`${user.userId}qwertyqweq`}
-                                    className="qwerty12"
-                                    type="checkbox"
-                                    checked={user.checked}
-                                    readOnly
-                                />
-                            )}
-                        </li>
-                    ))
+                    <li
+                        key={`${user.userId}qwerty`}
+                        onClick={
+                            isMulti
+                                ? onToggle.bind(null, user.userId)
+                                : handleClick.bind(null, user.userId)
+                        }
+                        className={`${
+                            user.checked ? "checked-user " : ""
+                        }users__list__item`}
+                    >
+                        <span key={user.userName} className="test123">
+                            {user.userName}
+                        </span>
+                        {isMulti && (
+                            <input
+                                key={`${user.userId}qwertyqweq`}
+                                className="qwerty12"
+                                type="checkbox"
+                                checked={user.checked}
+                                readOnly
+                            />
+                        )}
+                    </li>
+                ))
             ) : (
                 <span className="test123">Ничего не найдено</span>
-            )
-
-            }
-
+            )}
         </ul>
     );
 };
