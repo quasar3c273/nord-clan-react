f4dce5e53d4f37678f930116cb4defb9409f2157
Mikhail Lapitskiy <hiimisha@mail.ru>
2021-11-17 02:49:47 +0300

    stylelint is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.scss";
+import "./index.scss";
 import getUsers from "./get-data/getUsers";
 import EntryField from "./components/entry-field";
 import { infoAboutUsers } from "./types";
